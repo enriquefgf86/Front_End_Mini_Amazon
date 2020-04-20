53738e2d97e51ed8d9e8736487cfe5e55e50b860
Enrique Gordon <egordon.ubiqum@gmail.com>
2020-04-20 11:36:33 +0200

    adicionando barra a heroku link

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 import createPersistedState from "vuex-persistedstate";
 import router from "../router/index.js";
-const herokuUrl = "https://secure-castle-82044.herokuapp.com";
+const herokuUrl = "https://secure-castle-82044.herokuapp.com/";
 
 Vue.use(Vuex);
 export default new Vuex.Store({
@@ -169,7 +169,7 @@ export default new Vuex.Store({
     rateUpdater({ dispatch }, { product_id, ratePack }) {
       // console.log(product_id, ratePack);
 
-      fetch(herokuUrl + "/mini/all_products/user/product_rated/" + product_id, {
+      fetch(herokuUrl + "mini/all_products/user/product_rated/" + product_id, {
         credentials: "include",
         headers: {
           "Content-Type": "application/json",
@@ -197,7 +197,7 @@ export default new Vuex.Store({
 
     //------------metodo se supone almacene las posibles compras del usuario en su dashboard---------
     addProdToBuy({ dispatch }, { purchases }) {
-      fetch(herokuUrl + "/mini/all_products/allpurchase_view", {
+      fetch(herokuUrl + "mini/all_products/allpurchase_view", {
         credentials: "include",
         headers: {
           "Content-Type": "application/json",
@@ -224,7 +224,7 @@ export default new Vuex.Store({
 
     //----------adding products from provider vue tpo the generl products on sale-------------------
     addProductSale({ dispatch }, { currentProduct }) {
-      fetch(herokuUrl + "/mini/all_products/provider/product_added", {
+      fetch(herokuUrl + "mini/all_products/provider/product_added", {
         credentials: "include",
         headers: {
           "Content-Type": "application/json",
@@ -253,7 +253,7 @@ export default new Vuex.Store({
       // console.log(currentEditProduct, productId);
 
       fetch(
-        herokuUrl + "/mini/all_products/provider/product_edited/" + productId,
+        herokuUrl + "mini/all_products/provider/product_edited/" + productId,
         {
           credentials: "include",
           headers: {
@@ -283,7 +283,7 @@ export default new Vuex.Store({
     //-----------------------delete product-------------------------------
     deleteProviderProduct({ dispatch }, { currentDelProd, productId }) {
       // console.log(currentDelProd, productId);
-      fetch(herokuUrl + "/mini/all_products/delete/" + productId, {
+      fetch(herokuUrl + "mini/all_products/delete/" + productId, {
         credentials: "include",
         headers: {
           "Content-Type": "application/json",
@@ -338,7 +338,7 @@ export default new Vuex.Store({
     fetchUserIdProducts({ commit, getters }) {
       let userId = getters.getAllProducts.user.user_id;
       fetch(
-        herokuUrl + "/mini/all_products/one_selected/purchase_view/" + userId,
+        herokuUrl + "mini/all_products/one_selected/purchase_view/" + userId,
         {
           credentials: "include",
           method: "GET",
@@ -360,7 +360,7 @@ export default new Vuex.Store({
 
     //----------------------------vista Final del Board de compras hechas por el usuario-----------------
     fetchingAllPurchasesViewBoardUser({ commit }) {
-      fetch(herokuUrl + "/mini/all_products/user_dashboard/final_view", {
+      fetch(herokuUrl + "mini/all_products/user_dashboard/final_view", {
         credentials: "include",
         method: "GET",
       })
@@ -420,7 +420,7 @@ export default new Vuex.Store({
     getUserSignedUp({ commit, dispatch }, payload) {
       // console.log(payload);
 
-      fetch(herokuUrl + "/mini/all_product/registering", {
+      fetch(herokuUrl + "mini/all_product/registering", {
         credentials: "include",
         headers: {
           "Content-Type": "application/json",
@@ -451,7 +451,7 @@ export default new Vuex.Store({
 
     //-----------------------get user logged out------------------------------------------
     getuserLogOut({ commit }) {
-      fetch(herokuUrl + "/api/logout", {
+      fetch(herokuUrl + "api/logout", {
         credentials: "include",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
