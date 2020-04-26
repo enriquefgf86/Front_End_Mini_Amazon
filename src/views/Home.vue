b4a46ca0c5876f2423db55883594ee055ab7c86e
Enrique Gordon <egordon.ubiqum@gmail.com>
2020-04-26 20:07:48 +0200

    taking away los length

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,8 +42,7 @@
                 id="purchDrawer"
                 color="lime lighten-2 mb-5 ml-3"
                 x-small
-                v-if="getUserBoardPurch.all_user_purchases.length>0||getUserBoardPurch.all_user_purchases!=undefined&&
-                getUserBoardPurch.all_user_purchases.length>0"
+                
               >
                 Purchases
                 <img id="imgView" src="../assets/view.png" alt="view" />
@@ -83,8 +82,6 @@
 
           <router-link to="/myboard/buyerView">
             <v-btn
-              v-if="getUserBoardPurch.all_user_purchases.length>0||getUserBoardPurch.all_user_purchases!=undefined&&
-                getUserBoardPurch.all_user_purchases.length>0"
               class="hidden-sm-and-down"
               style="width:135px"
               id="bottomYourPurch"
